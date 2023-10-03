eb92e9bf92f2257d97fdaaf1f3864e850fe8ed75
Philipp Moreira <psmdeveloper@gmail.com>
2023-10-03 18:45:07 -0300

    feat ( Repositorio) Criado migration para permitir comandos DDL (Data Definition Language) atraves do ORM Entity Framework.

---
+++ Migrations/20231003210011_CriacaoBancoDadosOrganizadorTarefas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrilhaApiDesafio.Migrations
+{
+    public partial class CriacaoBancoDadosOrganizadorTarefas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tb_Tarefa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(200)", nullable: true),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tb_Tarefa", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tb_Tarefa");
+        }
+    }
+}
