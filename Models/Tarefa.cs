76d78add054ea1bed5d5359d9028a0d017fc62c1
Philipp Moreira <psmdeveloper@gmail.com>
2023-10-03 18:38:16 -0300

    feat (Application) Solucao Desafio.
    - Criacao de metodos de validacao da entidade;
    - Implementacao das funcionalidades CRUD através dos actionmethods/metodos de acao na controller;
    - Refatorado validacoes, abstraindo validacoes para classe de doiminio ao inves de mante-la na classe controller;

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace TrilhaApiDesafio.Models
 {
@@ -12,5 +8,47 @@ namespace TrilhaApiDesafio.Models
         public string Descricao { get; set; }
         public DateTime Data { get; set; }
         public EnumStatusTarefa Status { get; set; }
+
+
+        public bool TarefaEstaValida(out Dictionary<string, string> dicionarioMensagensValidacao)
+        {
+            dicionarioMensagensValidacao = new Dictionary<string, string>();
+
+            if(!DataTarefaEstaValida(out string mensagemRetorno))
+            {
+                dicionarioMensagensValidacao.Add($"{nameof(Data)}", mensagemRetorno);
+            }
+
+            if(!StatusTarefaEstaValida(out mensagemRetorno))
+            {
+                dicionarioMensagensValidacao.Add($"{nameof(Status)}", mensagemRetorno);
+            }            
+
+            return dicionarioMensagensValidacao.Count == 0;
+        }
+
+        public bool DataTarefaEstaValida(out string mensagemValidacao)
+        {
+            if (Data.Date == DateTime.MinValue.Date)
+            { 
+                mensagemValidacao = $"A data da tarefa não pode ser inválida ({DateTime.MinValue.Date.ToShortDateString()})";
+                return false;
+            }
+
+            mensagemValidacao = string.Empty;
+            return true;
+        }
+
+        public bool StatusTarefaEstaValida(out string mensagemValidacao)
+        {
+            if (!Enum.IsDefined<EnumStatusTarefa>(Status))
+            { 
+                mensagemValidacao = "O status da tarefa possui valor não esperado.";
+                return false;
+            }
+
+            mensagemValidacao = string.Empty;
+            return true;
+        }
     }
 }
